edddcaad73258ad0f4ee4fd9bc06397247310de0
Wihibfure <113610579+Wihibfure@users.noreply.github.com>
2025-04-18 03:41:03 +0200

    ok

---
+++ gruppehjemmeside/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+const members = [
+    {id:'1', name: 'Anna'},
+    {id:'2', name: 'Jonas'},
+    {id:'3', name: 'Emil'}
+]; 
+const Header = () => {
+    return (
+        <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
+        <div className="text-lg font-bold">Gruppe 3B</div>
+        <nav className="space-x-4">
+          <Link to="/">Hjem</Link>
+          {members.map((member) => (
+            <Link key={member.id} to={`/profil/${member.id}`}>
+              {member.name}
+            </Link>
+          ))}
+        </nav>
+      </header>
+    );
+  };
+  
+  export default Header;
