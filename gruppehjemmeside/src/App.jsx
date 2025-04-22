535a6afe8c03d50e7241de9561a6803c73bfac07
Wihibfure <113610579+Wihibfure@users.noreply.github.com>
2025-04-22 05:30:49 +0200

    Update App.jsx

--- gruppehjemmeside/src/App.jsx
+++ gruppehjemmeside/src/App.jsx
@@ -1,5 +1,6 @@
   import './App.css'
   import {Route, Routes} from 'react-router-dom';
+  import Forside from './components/Forside';
   import Header from './components/Header';
   import Profil from './components/Profil';
   import Home from './pages/Home';
@@ -15,6 +16,7 @@
     
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Forside />}></Route>
       <Route path="/profil/:id" element={<Profil />} />
       </Routes>
       </>
